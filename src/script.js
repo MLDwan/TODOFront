c321ea476a74f0bd3b79159a2511f4bf79454d66
Ivan Petrenko <ivanpetrenko.exceedteam@gmail.com>
2022-02-14 10:29:29 +0300

    render task

--- src/script.js
+++ src/script.js
@@ -2,6 +2,7 @@ let valueInput = "";
 let input = null;
 let flag = 0;
 let allTasks = [];
+let editInput = null;
 
 window.onload = async () => {
   input = document.getElementById("add-task");
@@ -16,12 +17,6 @@ window.onload = async () => {
 };
 
 const onClickButton = async () => {
-  allTasks.unshift({
-    text: valueInput,
-    isCheck: false,
-    flag: 0,
-  });
-
   const resp = await fetch("http://localhost:8000/createTask", {
     method: "POST",
     headers: {
@@ -33,12 +28,12 @@ const onClickButton = async () => {
       isCheck: false,
     }),
   });
-
+  
   let result = await resp.json();
   allTasks = result.data;
   valueInput = "";
   input.value = "";
-  
+
   render();
 };
 
@@ -48,22 +43,25 @@ const updateValue = (event) => {
 
 const render = () => {
   const content = document.getElementById("contentPage");
+  allTasks.reverse();
   allTasks.sort((a, b) => (b.isCheck === false) - (a.isCheck === false));
   while (content.firstChild) {
     content.removeChild(content.lastChild);
   }
 
   allTasks.map((item, index) => {
+
     let textIndex = allTasks[index].text;
-    let idIndex = allTasks[index].id;
+    let idIndex = allTasks[index]._id;
     let isCheckIndex = allTasks[index].isCheck;
+
     const container = document.createElement("div");
     container.id = `task-${index}`;
     container.className = "task-container";
     if (allTasks[index].flag === 1) {
-      const editInput = document.createElement("input");
-      editInput.addEventListener("change", updateValue);
+      editInput = document.createElement("input");
       editInput.type = "text";
+      editInput.addEventListener("change", updateValue);
       editInput.value = allTasks[index].text;
       container.appendChild(editInput);
       const acceptButton = document.createElement("button");
@@ -83,33 +81,36 @@ const render = () => {
     } else {
       const checkbox = document.createElement("input");
       checkbox.type = "checkbox";
-      checkbox.checked = item.isCheck;
-      checkbox.onclick = () => onChangeCheckBox(textIndex, isCheckIndex, idIndex);
+      checkbox.checked = allTasks[index].isCheck;
+      checkbox.onclick = () =>
+        onChangeCheckBox(textIndex, isCheckIndex, idIndex);
 
       container.appendChild(checkbox);
       const text = document.createElement("p");
       text.innerText = item.text;
       text.className = item.isCheck ? "text-task done-text" : "text-task";
-      container.className = item.isCheck ? "task-container-done" : "task-container";
+      container.className = item.isCheck
+        ? "task-container-done"
+        : "task-container";
       container.appendChild(text);
       const deleteButton = document.createElement("button");
-      
+
       if (allTasks[index].isCheck === false) {
         const editButton = document.createElement("button");
         editButton.onclick = () => editeFun(index);
         container.appendChild(editButton);
-        
+
         const imageEdit = document.createElement("img");
         imageEdit.src = "img/editor.svg";
         editButton.appendChild(imageEdit);
-      };
+      }
 
       deleteButton.onclick = () => deleteFun(idIndex);
       container.appendChild(deleteButton);
       const imageRemove = document.createElement("img");
       imageRemove.src = "img/remove.svg";
       deleteButton.appendChild(imageRemove);
-    };
+    }
 
     content.appendChild(container);
   });
@@ -119,7 +120,6 @@ const deleteFun = async (idIndex) => {
   const resp = await fetch(`http://localhost:8000/deleteTask?id=${idIndex}`, {
     method: "DELETE",
   });
-
   let result = await resp.json();
   allTasks = result.data;
 
@@ -146,7 +146,6 @@ const onChangeCheckBox = async (textIndex, isCheckIndex, idIndex) => {
       id: idIndex,
     }),
   });
-
   let result = await resp.json();
   allTasks = result.data;
 
